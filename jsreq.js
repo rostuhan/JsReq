d8a1729fe0b0e549654f32a2ceddd2b047c22a44
rostuhan <71077392+rostuhan@users.noreply.github.com>
2021-10-12 20:57:47 +0300

    Add files via upload

---
+++ jsreq.js
@@ -0,0 +1,19 @@
+// This code is not mine bc im bad at requests
+
+function Get(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
+
+function Post(url, data) {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify(data)
+    })
+}
+  
